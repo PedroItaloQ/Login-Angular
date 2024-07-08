4ce39c756158396cb6faa1daac76e6f6bcbe8ddd
PedroItaloQ <peedroitalo2906@gmail.com>
2024-07-07 21:37:42 -0300

    feat: Added login component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, LoginComponent]
 })
 export class AppComponent {
   title = 'login-angular';
