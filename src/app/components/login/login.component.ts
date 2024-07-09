fe7ba62423a3960923d73c4aa3641877a8a8a57c
PedroItaloQ <peedroitalo2906@gmail.com>
2024-07-08 23:21:46 -0300

    feat: created login function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginObj: Login;
+
+  constructor(private http: HttpClient) {
+    this.loginObj = new Login();
+  }
+
+  onLogin() {
+    debugger;
+    this.http.post('url', this.loginObj).subscribe((res:any) => {
+      if(res.result){
+        alert("Login com sucesso!");
+      } else {
+        alert(res.message);
+      };
+    });
+  };
+};
+
+export class Login
+{
+  EmailId: string;
+  Password: string;
+  constructor() {
+    this.EmailId = '';
+    this.Password = '';
+  }
 }
-
-
